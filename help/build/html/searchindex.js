cdb51450a0eb510a12e2ec8ae8d1bdc32b13fff0
Raphael <r.sulzer@student.tudelft.nl>
2019-02-20 16:39:11 +0100

    First commit

---
+++ help/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to PlanningToolClass\u2019s documentation!"],titleterms:{PlanningToolclass:[],document:0,indic:0,planningtoolclass:0,tabl:0,welcom:0}})
